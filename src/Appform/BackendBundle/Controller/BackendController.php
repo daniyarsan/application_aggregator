6163a079d9d1d7b8a5c2c24eb539d9e12beefcf7
imanali <iman.mamadiyev@gmail.com>
2015-04-28 14:56:43 +0600

    Backend - download files and remove files

--- src/Appform/BackendBundle/Controller/BackendController.php
+++ src/Appform/BackendBundle/Controller/BackendController.php
@@ -6,6 +6,7 @@ namespace Appform\BackendBundle\Controller;
 use Appform\BackendBundle\Form\ApplicantType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BackendController extends Controller{
@@ -60,6 +61,24 @@ class BackendController extends Controller{
         $em->flush();
 
         return $this->redirect($this->generateUrl('appform_backend_index'));
+    }
+
+    public function downloadFileAction($url)
+    {
+        $path = __DIR__.'/../../../../web/resume/';
+        if(file_exists($path.$url)) {
+            $content = file_get_contents($path.$url);
+            $response = new Response();
+            $response->headers->set('Content-Type', 'application/octet-stream');
+            $response->headers->set('Content-Disposition', 'attachment;filename="' . $url . '"');
+
+            $response->setContent($content);
+            return $response;
+        }
+        else{
+            header("HTTP/1.0 404 Not Found");
+            throw new NotFoundHttpException("HTTP/1.0 404 Not Found");
+        }
     }
 
 }
