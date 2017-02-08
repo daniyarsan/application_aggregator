652d159165ce545819f8a864f1c029fa15354dea
daniyar <daniyar.san@gmail.com>
2017-02-09 00:01:36 +0600

    Frontend added apply date field to pdf

--- src/Appform/FrontendBundle/Controller/DefaultController.php
+++ src/Appform/FrontendBundle/Controller/DefaultController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller {
 
@@ -296,6 +297,7 @@ class DefaultController extends Controller {
 			$data                  = $data ? $data : '';
 			$data                  = is_array( $data ) ? '' : $data;
 			$forPdf['candidateId'] = $applicant->getCandidateId();
+			$forPdf['applyDate'] = $applicant->getCreated()->format('d/m/Y');
 
 			$forPdf[ $key ] = $data;
 
