3d8dbfe38c70a1748d4df1ed60d6e02f571cbe94
daniyar <daniyar.san@gmail.com>
2018-03-31 00:26:43 +0600

    Frontend - Conversion Rate fix3

--- src/Appform/FrontendBundle/Entity/VisitorRepository.php
+++ src/Appform/FrontendBundle/Entity/VisitorRepository.php
@@ -30,7 +30,7 @@ class VisitorRepository extends \Doctrine\ORM\EntityRepository
 	protected function hasVisitor($ip, $refUrl) {
 
 		$time = new \DateTime('now');
-		$time->modify('-30 seconds');
+		$time->modify('-2 minute');
 
 		return $this->createQueryBuilder('v')
 				->select( 'count(v)' )
