d40054954e7f91ddfdb299a6a5a97aa2da6a2cb6
daniyar <daniyar.san@gmail.com>
2019-03-01 21:12:46 +0600

    Frontend - Fixed long disciplines in report 3

--- src/Appform/BackendBundle/Helper/FieldManager.php
+++ src/Appform/BackendBundle/Helper/FieldManager.php
@@ -25,8 +25,8 @@ class FieldManager
 		$helper = $this->container->get('helper');
 		$applicant['created'] = $applicant['created']->format('m/d/Y - H:i');
 		$applicant['state'] = $helper->getStates($applicant['state']);
-		$applicant['discipline'] = $helper->translateDiscipline($applicant['discipline']);
-		$applicant['specialtyPrimary'] = $helper->translateSpecialty($applicant['specialtyPrimary']);
+		$applicant['discipline'] = $helper->translateDisciplineShort($applicant['discipline']);
+		$applicant['specialtyPrimary'] = $helper->translateSpecialtyShort($applicant['specialtyPrimary']);
 		$applicant['specialtySecondary'] = $applicant['specialtySecondary'] ? $helper->translateSpecialty($applicant['specialtySecondary']) : false;
 		$applicant['yearsLicenceSp'] = $helper->getExpYears($applicant['yearsLicenceSp']);
 		$applicant['yearsLicenceSs'] = $applicant['yearsLicenceSs'] ? $helper->getExpYears($applicant['yearsLicenceSs']) : false;
